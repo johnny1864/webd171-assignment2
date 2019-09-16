ac30fabed6a6c33b5bccf29567e5a11504eb491e
johnny1864 <johnny1864@yahoo.com>
2019-09-15 21:30:28 -0700

    first commit

---
+++ src/body.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <div class="container-fluid">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
