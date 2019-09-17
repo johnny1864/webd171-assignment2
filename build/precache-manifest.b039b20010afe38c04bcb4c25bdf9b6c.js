77cd44c849d84562118ebcf1736c3fa99bc42dd6
johnny1864 <johnny1864@yahoo.com>
2019-09-16 17:07:58 -0700

    build prob

---
+++ build/precache-manifest.b039b20010afe38c04bcb4c25bdf9b6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11eeff5a0ecb16e03d8b18e414c14f0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1f6ea0e152de88e34fd",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "d2de791bada289a145e3",
+    "url": "/static/js/2.b21e5de5.chunk.js"
+  },
+  {
+    "revision": "f1f6ea0e152de88e34fd",
+    "url": "/static/js/main.e6a4bf7f.chunk.js"
+  },
+  {
+    "revision": "b0ad3679719f56c2f95a",
+    "url": "/static/js/runtime~main.1aaa1067.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
